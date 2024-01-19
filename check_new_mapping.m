958a3bdb43af400a6dd864bb518e906ffdd00f66
Alberto Battistel <alberto.battistel@rub.de>
2024-01-19 03:17:30 +0100

    some checks

--- check_new_mapping.m
+++ check_new_mapping.m
@@ -1,12 +1,16 @@
 
 
-ll = [10,125,200]; 
+ll = [1,125,200]; 
 
 for iii = 1:length(ll)
     figure(iii)
     clf
     hold on
-    plot(spec_Mtx_col(:,ll(iii))/max(abs(spec_Mtx_col(:,ll(iii)))))
-    plot(values(:,ll(iii))/max(abs(values(:,ll(iii)))))
+%     plot(spec_Mtx_col(:,ll(iii))/max(abs(spec_Mtx_col(:,ll(iii)))))
+%     plot(values(:,ll(iii))/max(abs(values(:,ll(iii)))))
+
+    plot(spec_Mtx_col(:,ll(iii)))
+    plot(values(:,ll(iii)))
+
     hold off
 end
