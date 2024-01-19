246276c18b7e0a82ce30f0e9012c276c271ecee0
Alberto Battistel <alberto.battistel@rub.de>
2024-01-19 03:18:28 +0100

    zigzag

---
+++ zigzag_matrix.m
@@ -0,0 +1,14 @@
+function [M] = zigzag_matrix(input_matrix)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+% https://stackoverflow.com/questions/3024939/matrix-zigzag-reordering
+
+M = input_matrix;
+
+ind = reshape(1:numel(M), size(M));         %# indices of elements
+ind = fliplr( spdiags( fliplr(ind) ) );     %# get the anti-diagonals
+ind(:,1:2:end) = flipud( ind(:,1:2:end) );  %# reverse order of odd columns
+ind(ind==0) = [];                           %# keep non-zero indices
+
+M(ind)  
+end
