530152114e2094cc5bb81ac7267a32c858397632
Alberto Battistel <alberto.battistel@rub.de>
2024-01-22 23:30:31 +0100

    make_DCT_subset accepts multidimensional

--- make_DCT_subset.m
+++ make_DCT_subset.m
@@ -1,21 +1,21 @@
 function DCT_subset = make_DCT_subset(elem_centers, coefficients_matrix)
 %DCT_subset = make_DCT_subset(elem_centers, coefficients_matrix)
-% returns the 2D rectangular DCT_subset calculated on elem_centers from the
+% returns the nD rectangular DCT_subset calculated on elem_centers from the
 % coefficients_matrix.
 
-x_max_coeff = max(coefficients_matrix(:,1));
-y_max_coeff = max(coefficients_matrix(:,2));
+max_coeffs = max(coefficients_matrix);
+
 DCT_subset = zeros(length(elem_centers), length(coefficients_matrix));
+
+normalization_ = 2/(prod(max_coeffs));
 
 for idk = 1:length(coefficients_matrix)
     a_jk = coefficients_matrix(idk,:);
-    normalization = 2/(x_max_coeff*y_max_coeff);
-    if a_jk(1) == 0
-        normalization = normalization/sqrt(2);
-    end
-    if a_jk(2) == 0
-        normalization = normalization/sqrt(2);
-    end
+    
+    zero_freqs = sum(a_jk == 1);
+    % each zero freq gets a 1/sqrt(2)
+    normalization = normalization_/(sqrt(2).^zero_freqs); 
+
     DCT_subset(:,idk) = normalization*cos(a_jk(1)*elem_centers(:,1)).*cos(a_jk(2)*elem_centers(:,2));
 end
 
