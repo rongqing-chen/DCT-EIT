7622e5c8a3a08fed10a72a9fef69d5f899d46b30
Alberto Battistel <alberto.battistel@rub.de>
2024-01-23 02:16:32 +0100

    it works with noise and different mesh

--- dct_3D.m
+++ dct_3D.m
@@ -1,7 +1,18 @@
+clear
+run reconstruction_3D
 
+%% create second model (avoid inverse crime)
+fmdl2 = ng_mk_cyl_models([3,1,0],[15,1,1.5,2],[0.1,0,0.05]); % 5293 nodes
+imdl2 = imdl;
+imdl2.fwd_model = fmdl2;
+imdl2.fwd_model.stimulation = imdl.fwd_model.stimulation;
+img_inv = mk_image(imdl2,1);
 
+% calc the Jacobian
+J = calc_jacobian(img_inv);
 
-elem_centers = recenter_model(fmdl)*pi;
+%%
+elem_centers = recenter_model(fmdl2)*pi;
 
 noise_ampl = 0.01;
 vi_noise = vi;
@@ -22,24 +33,25 @@ J_DCT = J* DCT_subset;
 
 R = eye(size(J_DCT,2));
 %%
-lambda = 1e-4; %without noise you can use 1e-6 or less
+lambda = 5e-4; %1e-4 with noise, 1e-6 or less without noise 
 dctCoeff = (J_DCT'*J_DCT + lambda.^2*R)\(J_DCT'*dva);
 
 %% inverse DCT
-recCond = DCT_subset*dctCoeff;
+reconstructed_elem = DCT_subset*dctCoeff;
 
 %% assigning element values
+imgr.fwd_model = fmdl2;
+imgr.fwd_model.stimulation = imdl.fwd_model.stimulation; 
+imgr.elem_data = reconstructed_elem;
 
-imgr.elem_data = recCond;
 
-
-figure(4)
+figure(5)
 subplot(1,2,1)
 show_fem(imgr);
 
 
 subplot(1,2,2)
-show_3d_slices(imgr, [1,1.9], [0.5],[0.5]);
+show_3d_slices(imgr, [1,1.9], [0.2],[0.5]);
 view(-14,13); axis tight; axis equal;
 
 
