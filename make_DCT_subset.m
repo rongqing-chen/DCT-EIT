7a6bf758620f9d955cc8c63354443369aa7453bc
Alberto Battistel <alberto.battistel@rub.de>
2024-01-23 01:16:09 +0100

    updated for nD

--- make_DCT_subset.m
+++ make_DCT_subset.m
@@ -1,7 +1,7 @@
 function DCT_subset = make_DCT_subset(elem_centers, coefficients_matrix)
 %DCT_subset = make_DCT_subset(elem_centers, coefficients_matrix)
 % returns the nD rectangular DCT_subset calculated on elem_centers from the
-% coefficients_matrix.
+% coefficients_matrix. elem_centers should between 0 and pi.
 
 max_coeffs = max(coefficients_matrix);
 
@@ -15,8 +15,8 @@ for idk = 1:length(coefficients_matrix)
     zero_freqs = sum(a_jk == 1);
     % each zero freq gets a 1/sqrt(2)
     normalization = normalization_/(sqrt(2).^zero_freqs); 
-
-    DCT_subset(:,idk) = normalization*cos(a_jk(1)*elem_centers(:,1)).*cos(a_jk(2)*elem_centers(:,2));
+    DCT_subset(:,idk) = normalization*prod(cos(elem_centers.*a_jk),2);
+%     DCT_subset(:,idk) = normalization*cos(a_jk(1)*elem_centers(:,1)).*cos(a_jk(2)*elem_centers(:,2));
 end
 
 end
